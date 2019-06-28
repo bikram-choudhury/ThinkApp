a847a99651f4bc97d02a9951f8d2b7cd525a86ab
Bikram Choudhury <bikram.choudhury527@gmail.com>
2019-06-28 12:42:39 +0530

    [IMPL] Question Creation view

--- src/app/question-master/question-master.component.ts
+++ src/app/question-master/question-master.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-master.component.scss']
 })
 export class QuestionMasterComponent implements OnInit {
-
+  questionType: string;
+  choiceLength: any[] = new Array(4);
+  topicList: string[] = ['Angular', 'React', 'Node', 'Javascript', 'Cricket', 'Footballs']
+  selectedTopic: string = 'Topic';
+  questionModel: {[key: string]: string} = {
+    topicName: '',
+    qType: '',
+    question: ''
+  };
   constructor() { }
 
   ngOnInit() {
+  }
+  selectTopic(topic: string) {
+    this.selectedTopic = topic.toLowerCase();
+  }
+  submit() {
+    console.log(this.questionModel);
   }
 
 }

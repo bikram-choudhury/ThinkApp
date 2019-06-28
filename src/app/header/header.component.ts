8109886409549a633c49a5fce28729c20baa4ddb
Bikram Choudhury <bikram.choudhury527@gmail.com>
2019-06-28 10:50:49 +0530

    Component interaction

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  @Input() Header: string;
   constructor() { }
 
   ngOnInit() {

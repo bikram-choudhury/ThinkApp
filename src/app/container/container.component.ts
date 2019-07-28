9889ffe3523272f6aa2dcb897cef8efdc420bd87
Bikram Choudhury <bikram.choudhury527@gmail.com>
2019-07-28 15:47:48 +0530

    1. [Fix] load style only for required components 2. [Fix] route url changes in sidebar

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
-  styleUrls: ['./container.component.scss']
+  styleUrls: ['./container.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ContainerComponent implements OnInit {
 

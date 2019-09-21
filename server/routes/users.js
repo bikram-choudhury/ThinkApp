293d29a528cb0008afcfe93c56ef2d5b01173ec4
Bikram Choudhury <bikram.choudhury527@gmail.com>
2019-09-21 12:18:22 +0530

    Introduction to async package

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const dummy = require('./../controller/dummy-controller');
+const async = require('async');
+const fetch = require('node-fetch');
 
 router.get('/fetch', (request, response) => {
     response.json({
@@ -26,5 +28,76 @@ router.get('/fetch/async/:userID?', async (request, response) => {
         response.status(500).send(error.message);
     }
 });
+
+const fetchUserDataFromFakeServer = (request, response, next) => {
+    const userID = request.params.userID;
+    async.parallel([
+        (callback) => {
+            fetch(`https://jsonplaceholder.typicode.com/users/${userID}`)
+            .then(response => response.json())
+            .then(userData => {
+                callback(null, userData)
+            })
+            .catch(error => {
+                callback(error)
+            })
+        },
+        (callback) => {
+            fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userID}`)
+            .then(response => response.json())
+            .then(postData => {
+                callback(null, postData)
+            })
+            .catch(error => {
+                callback(error)
+            })
+        }
+    ], (error, result) => {
+        if(error) {
+            response.status(500).send(error);
+        }
+        request.userData = result;
+        next()
+    })
+};
+
+const formatUserData = (request, response) => {
+    const [UserDetails, UserPosts] = request.userData;
+    const {id, name, email, website} = UserDetails;
+    const user = {id, name, email, website};
+    user.posts = UserPosts.map(post => {
+        delete post.userId;
+        return post;
+    });
+    response.json(user);
+};
+
+const fetchCommentsFromFakeServer = (request, response) => {
+    async.waterfall([
+        callback => {
+            const postID = request.params.postID;
+            callback(null, postID);
+        },
+        (postID, callback) => {
+            fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postID}`)
+            .then(response => response.json())
+            .then(comments => {
+                callback(null, comments)
+            })
+            .catch(error => {
+                callback(error)
+            })
+        }
+    ], (error, result) => {
+        if(error) {
+            response.status(500).send(error);
+        }
+        response.json(result);
+    })
+}
+
+router.get('/fetch/async-package/parallel/:userID', fetchUserDataFromFakeServer, formatUserData)
+
+router.get('/fetch/async-package/waterfall/:postID', fetchCommentsFromFakeServer)
 
 module.exports = router;
